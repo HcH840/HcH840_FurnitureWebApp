5bb0505d91dd49615d3fe2f1db5f16b10d0d92ea
HcH840 <136321128+HcH840@users.noreply.github.com>
2023-07-03 02:09:12 -0800

    Update WeatherForecast.cs

--- HcH840_FurnitureWebApp/WeatherForecast.cs
+++ HcH840_FurnitureWebApp/WeatherForecast.cs
@@ -1,12 +1,1 @@
-namespace HcH840_FurnitureWebApp;
 
-public class WeatherForecast
-{
-    public DateTime Date { get; set; }
-
-    public int TemperatureC { get; set; }
-
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
-}
